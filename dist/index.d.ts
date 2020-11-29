2f6bded05493dbd372a8fddfd48545a86716aabe
C0C41N <0ct41n@gmail.com>
2020-11-29 20:56:57 +0500

    Initial commit

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Express } from 'express';
-import serverless = require('serverless-http');
-export declare const createNestServer: (expressInstance: Express) => Promise<import("@nestjs/common").INestApplication>;
-export declare const handler: serverless.Handler;
